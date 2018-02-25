de1432d0ade679f701bd71cc4baacb820cced8a7
Brian Lai <brianlai30@outlook.com>
2018-02-25 02:08:16 -0800

    Added Projectile.cs

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -14,4 +14,11 @@ public class EnemyBehavior : MonoBehaviour {
 		
 	}
 
+    void OnTriggerEnter2D(Collider2D collider) {
+        Projectile missile = collider.gameObject.GetComponent<Projectile>();
+        if (missile) {
+            Debug.Log("Hit by a projectile");
+        }
+    }
+
 }
