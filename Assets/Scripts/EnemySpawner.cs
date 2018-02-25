c0fe439e4ce17365332f9df966aaf8ff042e1ca1
Brian Lai <brianlai30@outlook.com>
2018-02-24 23:23:30 -0800

    Added EnemySpawner.cs

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start() {
+		Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+}
