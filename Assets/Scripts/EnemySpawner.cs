abdfdfc711c73a8492a8a5352adbbf5671b504a0
Brian Lai <brianlai30@outlook.com>
2018-02-25 00:55:06 -0800

    Enemies will spawn at their positions

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,8 +9,8 @@ public class EnemySpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start() {
         foreach(Transform child in transform) {
-            GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-            enemy.transform.parent = transform;
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
         }
 	}
 	
