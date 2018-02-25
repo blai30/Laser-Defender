f16971b40f5668c081c19efc0570aac455ef7a55
Brian Lai <brianlai30@outlook.com>
2018-02-25 02:15:01 -0800

    Enemy has health and can die

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
 
+    public float health;
+
     void OnTriggerEnter2D(Collider2D collider) {
         Projectile missile = collider.gameObject.GetComponent<Projectile>();
         if (missile) {
             health -= missile.GetDamage();
+            if (health <= 0) {
+                Destroy(gameObject);
+            }
             Debug.Log("Hit by a projectile");
         }
     }
