0799faf47e8e6303241fcc05441e2c4dc581822c
Brian Lai <brianlai30@outlook.com>
2018-02-25 22:45:33 -0800

    Music plays in all scenes, added starfields to start and end

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -17,11 +17,27 @@ public class MusicPlayer : MonoBehaviour {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
             music = GetComponent<AudioSource>();
+            music.clip = startClip;
+            music.loop = true;
+            music.Play();
 		}
 	}
 
     void OnLevelWasLoaded(int level) {
         Debug.Log("MusicPlayer: loaded level " + level);
+        music.Stop();
+
+        if (level == 0) {
+            music.clip = startClip;
+        }
+        if (level == 1) {
+            music.clip = gameClip;
+        }
+        if (level == 2) {
+            music.clip = endClip;
+        }
+        music.loop = true;
+        music.Play();
     }
 
 }
