3a204d677bca8bb3bdaebaa1d3fd1606ea173a9f
Brian Lai <brianlai30@outlook.com>
2018-02-25 18:36:39 -0800

    Added ScoreKeeper.cs

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public int score = 0;
+
+    void Score(int points) {
+        score += points;
+    }
+
+    void Reset() {
+        score = 0;
+    }
+
+}
