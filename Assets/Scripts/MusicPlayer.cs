4e883a6103cb494cf4ee5cca2eceb6db595f30b8
Brian Lai <brianlai30@outlook.com>
2018-02-25 22:34:20 -0800

    Changed up how music is played

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,16 +2,26 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
+
 	static MusicPlayer instance = null;
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+    private AudioSource music;
 	
-	void Start () {
+	void Start() {
 		if (instance != null && instance != this) {
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
 		}
-		
 	}
+
+    void OnLevelWasLoaded(int level) {
+        Debug.Log("MusicPlayer: loaded level " + level);
+    }
+
 }
